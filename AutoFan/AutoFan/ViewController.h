c67b8298be8874193f77d1ed3ddc7074e1629190
Shawn Zhang <shawnzsl130@gmail.com>
2022-11-27 02:42:07 -0800

    99% of the app UI is completed

--- AutoFan/AutoFan/ViewController.h
+++ AutoFan/AutoFan/ViewController.h
@@ -7,11 +7,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    int current_level;
+    int list_size;
+}
 
 @property (weak, nonatomic) IBOutlet UIView *autoDefaultView;
 @property (weak, nonatomic) IBOutlet UIView *autoManualView;
+@property (weak, nonatomic) IBOutlet UIView *manualView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segControl;
+@property (weak, nonatomic) IBOutlet UIView *manualLevel;
+
+- (void) manualLevelUpdate:(int)level;
+- (void) alert:(NSString*)str;
+
+@property (weak, nonatomic) IBOutlet UITextField *AutoManualTempFrom;
+@property (weak, nonatomic) IBOutlet UITextField *AutoManualTempTo;
+@property (weak, nonatomic) IBOutlet UITextField *AutoManualRPM;
+@property (weak, nonatomic) IBOutlet UIView *AutoManualList;
 
 @end
 
--- AutoFan/AutoFan/ViewController.m
+++ AutoFan/AutoFan/ViewController.m
@@ -8,22 +8,106 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
+@synthesize AutoManualTempTo;
+@synthesize AutoManualTempFrom;
+@synthesize AutoManualRPM;
+@synthesize AutoManualList;
+
+- (void) manualLevelUpdate:(int)level {
+    self.manualLevel.subviews[current_level].backgroundColor = UIColor.systemGray3Color;
+    self.manualLevel.subviews[level].backgroundColor = UIColor.systemRedColor;
+    current_level = level;
+    list_size = 0;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.manualView.hidden = true;
+    current_level = 0;
+    [self manualLevelUpdate:current_level];
 }
-
 
 - (IBAction)autoDefaultChange:(id)sender {
     if (self.segControl.selectedSegmentIndex == 1)
         self.autoDefaultView.hidden = true;
     else
         self.autoDefaultView.hidden = false;
+}
+
+- (IBAction)manualOnClickFromAutoManual:(id)sender {
+    self.manualView.hidden = false;
+}
+
+- (IBAction)manualOnClickFromAutoDefault:(id)sender {
+    self.manualView.hidden = false;
+}
+
+- (IBAction)autoOnClickFromManual:(id)sender {
+    self.manualView.hidden = true;
+}
+
+- (IBAction)manualLevel1OnSelect:(id)sender {
+    [self manualLevelUpdate:0];
+}
+
+- (IBAction)manualLevel2OnSelect:(id)sender {
+    [self manualLevelUpdate:1];
+}
+
+- (IBAction)manualLevel3OnSelect:(id)sender {
+    [self manualLevelUpdate:2];
+}
+
+- (IBAction)manualLevel4OnSelect:(id)sender {
+    [self manualLevelUpdate:3];
+}
+
+- (IBAction)manualLevel5OnSelect:(id)sender {
+    [self manualLevelUpdate:4];
+}
+
+- (IBAction)AutoManualAddOnClick:(id)sender {
+    if (list_size >= 8) {
+        [self alert:@"Maximum manual setting reached, please clear before adding anymore setting."];
+        return;
+    }
+    
+    if ([AutoManualTempFrom.text isEqualToString:@""] || [AutoManualTempTo.text isEqualToString:@""] || [AutoManualRPM.text isEqualToString:@""]) {
+        [self alert:@"Empty input"];
+        return;
+    }
+    
+    NSString *str = [NSString stringWithFormat:@"From %.01f to %.01f with RPM: %i", [AutoManualTempFrom.text floatValue], [AutoManualTempTo.text floatValue], [AutoManualRPM.text intValue]];
+    
+    UILabel *myLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, (list_size++)*25, 280, 40)];
+    [myLabel setBackgroundColor:[UIColor clearColor]];
+    [myLabel setText:str];
+    [[self AutoManualList] addSubview:myLabel];
+}
+
+- (IBAction)clearOnClick:(id)sender {
+    list_size = 0;
+    for (UIView *view in AutoManualList.subviews) {
+        [view removeFromSuperview];
+    }
+}
+
+- (void) alert:(NSString*)str {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Warning"
+                               message:str
+                               preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {}];
+
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 
